4855450d20a20305263eccdeb48d1325dd68e8bb
周健邦 <3510537395@qq.com>
2023-06-07 22:30:30 +0800

    完成了添加设备和编辑设备功能

--- src/views/DeviceManage/DeviceInfo/Interface/form.d.ts
+++ src/views/DeviceManage/DeviceInfo/Interface/form.d.ts
@@ -8,11 +8,12 @@ export interface searchForm {
 }
 
 export interface dialogForm {
+	id: ''
 	name: string
 	m_nPort: number
 	m_strIp: string
 	m_strUser: string
 	m_strPassword: string
-	status: string
 	type: string
+	status: string
 }
--- src/views/DeviceManage/DeviceInfo/index.vue
+++ src/views/DeviceManage/DeviceInfo/index.vue
@@ -3,6 +3,7 @@ import type { FormInstance, FormRules } from 'element-plus'
 import { Edit, Delete, Search } from '@element-plus/icons-vue'
 import { useDeviceStore } from '@/stores/modules/device'
 import type { searchForm, dialogForm } from './Interface/form'
+import { ElMessage } from 'element-plus'
 
 const deviceStore = useDeviceStore()
 
@@ -10,7 +11,7 @@ const deviceStore = useDeviceStore()
 const searchForm = ref<searchForm>({
 	name: '',
 	m_strIp: '',
-	m_nPort: 37777,
+	m_nPort: 0,
 	id: '',
 	type: '',
 	status: ''
@@ -50,13 +51,17 @@ const handleResetBtnClick = () => {
 }
 // 搜索栏搜索按钮回调
 const handleSearchBtnClick = () => {
-	deviceStore.getAllDeviceInfoAction(searchForm.value)
+	currentPage.value = 1
+	deviceStore.getAllDeviceInfoAction(
+		searchForm.value,
+		currentPage.value,
+		pageSize.value
+	)
 }
 
 // 分页器数据
 const currentPage = ref(1)
 const pageSize = ref(10)
-const total = ref(0)
 /*
  * 分页大小改变回调
  */
@@ -74,16 +79,11 @@ const handleCurrentChange = (newPage: number) => {
 const ruleFormRef = ref<FormInstance>()
 
 // 获取表格数据
-if (typeof searchForm.value.m_nPort === 'string') {
-	const newParams = ref()
-	newParams.value = {
-		...searchForm.value,
-		m_nPort: Number(searchForm.value.m_nPort)
-	}
-	deviceStore.getAllDeviceInfoAction(newParams.value)
-}
-
-deviceStore.getAllDeviceInfoAction(searchForm.value)
+deviceStore.getAllDeviceInfoAction(
+	searchForm.value,
+	currentPage.value,
+	pageSize.value
+)
 // 编辑框的类型（添加和编辑）
 const dialogType = ref('')
 /**
@@ -92,13 +92,14 @@ const dialogType = ref('')
 const DialogVisible = ref(false)
 // dialogForm表单数据
 const dialogForm = ref<dialogForm>({
+	id: '',
 	name: '',
 	m_nPort: 0,
 	m_strIp: '',
 	m_strUser: '',
 	m_strPassword: '',
-	status: '',
-	type: '0'
+	type: '0',
+	status: ''
 })
 /*
  * 添加设备按钮回调
@@ -118,33 +119,70 @@ const handleEditBtnClick = (rowData: any) => {
 		dialogForm.value = JSON.parse(JSON.stringify(rowData))
 	})
 }
+// 表格删除按钮回调
+const handleDeleteBtnClick = async (id: string) => {
+	let arr = []
+	arr.push(id)
+	const code = await deviceStore.delDeviceAction(arr)
+	if (code === 200) {
+		ElMessage({
+			message: '删除成功',
+			type: 'success'
+		})
+		deviceStore.getAllDeviceInfoAction(
+			searchForm.value,
+			currentPage.value,
+			pageSize.value
+		)
+	}
+}
 /*
  * 对话框关闭回调
  */
-const test = ref()
+const dialogFormRef = ref()
 const handleEditClose = () => {
-	// setTimeout(() => {
 	dialogType.value = ''
 	nextTick(() => {
-		test.value.resetFields()
+		dialogFormRef.value.resetFields()
 	})
-	console.log(11111111)
-	// dialogForm.value = {
-	// 	name: '',
-	// 	m_nPort: 0,
-	// 	m_strIp: '',
-	// 	m_strUser: '',
-	// 	m_strPassword: '',
-	// 	status: '',
-	// 	type: '0'
-	// }
-	// }, 500)
 	DialogVisible.value = false
 }
 /*
  * 对话框确认按钮回调
  */
-const handleConfirmClick = () => {}
+const handleConfirmClick = async () => {
+	if (dialogType.value === 'add') {
+		const code = await deviceStore.addDeviceAction(dialogForm.value)
+		if (code == 200) {
+			DialogVisible.value = false
+			dialogFormRef.value.resetFields()
+			deviceStore.getAllDeviceInfoAction(
+				searchForm.value,
+				currentPage.value,
+				pageSize.value
+			)
+			ElMessage({
+				message: '添加成功',
+				type: 'success'
+			})
+		}
+	} else {
+		const code = await deviceStore.editDeviceAction(dialogForm.value)
+		if (code == 200) {
+			DialogVisible.value = false
+			dialogFormRef.value.resetFields()
+			deviceStore.getAllDeviceInfoAction(
+				searchForm.value,
+				currentPage.value,
+				pageSize.value
+			)
+			ElMessage({
+				message: '编辑成功',
+				type: 'success'
+			})
+		}
+	}
+}
 </script>
 
 <template>
@@ -154,7 +192,7 @@ const handleConfirmClick = () => {}
 			<el-form :model="searchForm" ref="searchFormRef">
 				<!-- 第一行 -->
 				<el-row :gutter="20" class="row-first">
-					<el-col :span="8">
+					<el-col :span="6">
 						<el-form-item label="设备ID:" prop="id">
 							<el-input
 								v-model:model-value="searchForm.id"
@@ -162,7 +200,7 @@ const handleConfirmClick = () => {}
 							/>
 						</el-form-item>
 					</el-col>
-					<el-col :span="8">
+					<el-col :span="6">
 						<el-form-item label="设备名称:" prop="name">
 							<el-input
 								v-model:model-value="searchForm.name"
@@ -170,7 +208,7 @@ const handleConfirmClick = () => {}
 							/>
 						</el-form-item>
 					</el-col>
-					<el-col :span="8">
+					<el-col :span="6">
 						<el-form-item label="端口号:" prop="m_nPort">
 							<el-input
 								v-model.number="searchForm.m_nPort"
@@ -178,10 +216,7 @@ const handleConfirmClick = () => {}
 							/>
 						</el-form-item>
 					</el-col>
-				</el-row>
-				<!-- 第二行 -->
-				<el-row :gutter="20">
-					<el-col :span="8">
+					<el-col :span="6">
 						<el-form-item label="IP地址:" prop="m_strIp">
 							<el-input
 								v-model:model-value="searchForm.m_strIp"
@@ -189,7 +224,10 @@ const handleConfirmClick = () => {}
 							/>
 						</el-form-item>
 					</el-col>
-					<el-col :span="8">
+				</el-row>
+				<!-- 第二行 -->
+				<el-row :gutter="20">
+					<el-col :span="6">
 						<el-form-item label="类型:" prop="type"
 							><el-select v-model="searchForm.type" placeholder="请选择类型">
 								<el-option
@@ -201,7 +239,19 @@ const handleConfirmClick = () => {}
 							</el-select>
 						</el-form-item>
 					</el-col>
-					<el-col :span="8">
+					<el-col :span="6">
+						<el-form-item label="类型:" prop="status"
+							><el-select v-model="searchForm.status" placeholder="请选择状态">
+								<el-option
+									v-for="item in statusSelectData"
+									:key="item.value"
+									:label="item.label"
+									:value="item.value"
+								/>
+							</el-select>
+						</el-form-item>
+					</el-col>
+					<el-col :span="12">
 						<div class="operation">
 							<el-button
 								:icon="Search"
@@ -227,7 +277,7 @@ const handleConfirmClick = () => {}
 		<div class="card table-main">
 			<el-table :data="deviceStore.deviceList" border style="width: 100%">
 				<!-- 索引 -->
-				<el-table-column type="index" label="#" width="180" align="center" />
+				<el-table-column type="index" label="#" width="50" align="center" />
 				<!-- 设备名称 -->
 				<el-table-column
 					prop="name"
@@ -235,12 +285,21 @@ const handleConfirmClick = () => {}
 					width="180"
 					align="center"
 				/>
+				<!-- 设备ID -->
+				<el-table-column prop="id" label="设备ID" width="180" align="center" />
+				<!-- 用户名 -->
+				<el-table-column
+					prop="m_strUser"
+					label="用户名"
+					width="100"
+					align="center"
+				/>
 				<!-- 端口号 -->
 				<el-table-column prop="m_nPort" label="端口号" align="center" />
 				<!-- IP地址 -->
 				<el-table-column prop="m_strIp" label="IP地址" align="center" />
 				<!-- 状态 -->
-				<el-table-column prop="m_strIp" label="状态" align="center">
+				<el-table-column prop="m_strIp" label="状态" align="center" width="80">
 					<template #default="scope">
 						<el-tag effect="dark" type="success" v-if="scope.row.status === '1'"
 							>连接</el-tag
@@ -249,7 +308,12 @@ const handleConfirmClick = () => {}
 					</template>
 				</el-table-column>
 				<!-- 出入类型 -->
-				<el-table-column prop="type" label="出入类型" align="center">
+				<el-table-column
+					prop="type"
+					label="出入类型"
+					align="center"
+					width="100"
+				>
 					<template #default="scope">
 						<el-tag effect="plain" type="success" v-if="scope.row.type === '1'">
 							进
@@ -268,7 +332,16 @@ const handleConfirmClick = () => {}
 								@click="handleEditBtnClick(scope.row)"
 								>编辑</el-button
 							>
-							<el-button text type="primary" :icon="Delete">删除</el-button>
+							<el-popconfirm
+								title="是否删除该设备?"
+								icon-color="#ff4949"
+								icon="WarnTriangleFilled"
+								@confirm="handleDeleteBtnClick(scope.row.id)"
+							>
+								<template #reference>
+									<el-button text type="primary" :icon="Delete">删除</el-button>
+								</template>
+							</el-popconfirm>
 						</div>
 					</template>
 				</el-table-column>
@@ -279,7 +352,7 @@ const handleConfirmClick = () => {}
 				v-model:page-size="pageSize"
 				:page-sizes="[10, 20, 30, 40]"
 				layout="total, sizes, prev, pager, next, jumper"
-				:total="total"
+				:total="deviceStore.total"
 				@size-change="handleSizeChange"
 				@current-change="handleCurrentChange"
 			/>
@@ -297,27 +370,35 @@ const handleConfirmClick = () => {}
 				:model="dialogForm"
 				label-position="left"
 				label-width="85px"
-				ref="test"
+				ref="dialogFormRef"
 			>
+				<el-form-item label="设备ID：" prop="id" v-show="dialogType !== 'edit'">
+					<el-input v-model="dialogForm.id" placeholder="请输入设备ID" />
+				</el-form-item>
 				<el-form-item label="设备名称：" prop="name">
-					<el-input v-model="dialogForm.name" />
+					<el-input v-model="dialogForm.name" placeholder="请输入设备名称" />
 				</el-form-item>
 				<el-form-item label="端口号：" prop="m_nPort">
-					<el-input v-model="dialogForm.m_nPort" />
+					<el-input
+						v-model.number="dialogForm.m_nPort"
+						placeholder="请输入端口号"
+					/>
 				</el-form-item>
 				<el-form-item label="IP地址：" prop="m_strIp">
-					<el-input v-model="dialogForm.m_strIp" />
+					<el-input v-model="dialogForm.m_strIp" placeholder="请输入ID地址" />
 				</el-form-item>
 				<el-form-item label="用户名：" prop="m_strUser">
-					<el-input v-model="dialogForm.m_strUser" />
+					<el-input v-model="dialogForm.m_strUser" placeholder="请输入用户名" />
 				</el-form-item>
 				<el-form-item label="密码：" prop="m_strPassword">
-					<el-input v-model="dialogForm.m_strPassword" />
+					<el-input
+						v-model="dialogForm.m_strPassword"
+						placeholder="请输入密码"
+					/>
 				</el-form-item>
 				<el-form-item label="类型：" prop="status">
 					<el-switch
 						v-model="dialogForm.type"
-						class="ml-2"
 						inline-prompt
 						style="
 							--el-switch-on-color: #13ce66;
