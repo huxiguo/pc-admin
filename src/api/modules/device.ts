f851b8e2acd55f37be779919264c0fae572e48e9
周健邦 <3510537395@qq.com>
2023-06-08 22:24:31 +0800

    完成了设备管理中的连接设备页面大致布局，以及按需查询、在当前页选中连接中或断开的设备功能

--- src/api/modules/device.ts
+++ src/api/modules/device.ts
@@ -24,6 +24,16 @@ export const editDevice = (params: Device.ReqEditDevice) => {
 }
 
 // 删除设备
-export const delDevice = (params: any) => {
+export const delDevice = (params: Device.ReqDelDevice) => {
 	return http.delete<any>('/device/delDevice', params)
+}
+
+// 启动设备
+export const runDevice = (params: Device.ReqRunOrCloseDevice) => {
+	return http.post<any>('/device/runDevice', params)
+}
+
+// 关闭设备
+export const closeDevice = (params: Device.ReqRunOrCloseDevice) => {
+	return http.post<any>('/device/closeDevice', params)
 }
---
+++ src/views/DeviceManage/DeviceRun/Interface/form.d.ts
@@ -0,0 +1,19 @@
+export interface searchForm {
+	name: string
+	m_nPort: number
+	m_strIp: string
+	type: string
+	id: string
+	status: string
+}
+
+export interface device {
+	id: ''
+	name: string
+	m_nPort: number
+	m_strIp: string
+	m_strUser: string
+	m_strPassword: string
+	type: string
+	status: string
+}
