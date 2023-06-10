1abf7cc74e39c7d250af60666529796458c75b00
周健邦 <3510537395@qq.com>
2023-06-10 23:33:59 +0800

    完成单位管理页面布局及添加和删除单位、导出单位数据功能

--- components.d.ts
+++ components.d.ts
@@ -47,6 +47,7 @@ declare module '@vue/runtime-core' {
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTag: typeof import('element-plus/es')['ElTag']
+    ElUpload: typeof import('element-plus/es')['ElUpload']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
