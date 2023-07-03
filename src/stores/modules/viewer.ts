c98e31490505e8d2d8d6777a7a88dd450f82defe
周健邦 <3510537395@qq.com>
2023-07-03 15:43:36 +0800

    完成监视人页面中绑定和解绑被监视人功能

--- src/stores/modules/viewer.ts
+++ src/stores/modules/viewer.ts
@@ -1,7 +1,13 @@
 import { defineStore } from 'pinia'
 import piniaPersistConfig from '@/config/piniaPersist'
 import type { Viewer } from '@/global/viewer'
-import { getAllViewer, deleteViewer } from '@/api/modules/viewer'
+import {
+	getAllViewer,
+	deleteViewer,
+	getViewerBindUser,
+	deleteBeUser,
+	addBeUser
+} from '@/api/modules/viewer'
 
 export const useViewerStore = defineStore(
 	'viewer',
@@ -24,14 +30,30 @@ export const useViewerStore = defineStore(
 		async function deleteViewerAction(params: Viewer.ReqDelViewer) {
 			await deleteViewer(params)
 		}
+		// 获取监视人绑定的用户
+		async function getViewerBindUserAction(viewId: number) {
+			const { result } = await getViewerBindUser(viewId)
+			return result
+		}
+		// 解绑被监视人
+		async function deleteBeUserAction(params: Viewer.ReqDeleteOrAddBeUser) {
+			await deleteBeUser(params)
+		}
+		// 绑定被监视人
+		async function addBeUserAction(params: Viewer.ReqDeleteOrAddBeUser) {
+			await addBeUser(params)
+		}
 		return {
 			total,
 			viewerList,
 			getAllViewerAction,
-			deleteViewerAction
+			deleteViewerAction,
+			getViewerBindUserAction,
+			deleteBeUserAction,
+			addBeUserAction
 		}
 	},
 	{
-		persist: piniaPersistConfig('device', [])
+		persist: piniaPersistConfig('viewer', [])
 	}
 )
